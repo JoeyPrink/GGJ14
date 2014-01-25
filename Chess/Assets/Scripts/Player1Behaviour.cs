1664324f2fd846f5f86b2fcb2fbd43028cc6d631
JoeyPrink <joeyprink@gmail.com>
2014-01-25 10:43:47 +0100

    two players

---
+++ Chess/Assets/Scripts/Player1Behaviour.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Behaviour : MonoBehaviour {
+	
+	
+	
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal1");
+		float inputY = Input.GetAxis("Vertical1");
+
+		print (inputX);
+
+		rigidbody2D.AddForce (Vector2.right * inputX);
+		
+
+		
+		
+		
+		// 4 - Movement per direction
+		/*movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+	*/	
+	}
+
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+	}
+	
+}
---
+++ Chess/Assets/Scripts/Player2Behaviour.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2Behaviour : MonoBehaviour {
+	
+	
+	
+	/// <summary>
+	/// 1 - The speed of the ship
+	/// </summary>
+	public Vector2 speed = new Vector2(50, 50);
+	
+	// 2 - Store the movement
+	private Vector2 movement;
+	
+	void Update()
+	{
+		// 3 - Retrieve axis information
+		float inputX = Input.GetAxis("Horizontal2");
+		float inputY = Input.GetAxis("Vertical2");
+		
+		
+		/*	if(Input.GetAxisRaw("Joystick 1 X axis")> 0.3|| Input.GetAxisRaw("Joystick 1 X axis") < -0.3)
+		{
+			inputX = Input.GetAxisRaw("Joystick 1 X axis");
+		}
+		
+		if(Input.GetAxisRaw("Y axis")> 0.3|| Input.GetAxisRaw("Y axis") < -0.3)
+		{
+			 inputY = Input.GetAxisRaw("Y axis");
+		}
+
+
+		/*
+		if(Input.GetAxisRaw("3rd axis")> 0.3|| Input.GetAxisRaw("3rd axis") < -0.3)
+		{
+			currentAxis = "3rd axis";
+			axisInput = Input.GetAxisRaw("3rd axis");
+		}
+		
+		if(Input.GetAxisRaw("4th axis")> 0.3|| Input.GetAxisRaw("4th axis") < -0.3)
+		{
+			currentAxis = "4th axis";
+			axisInput = Input.GetAxisRaw("4th axis");
+		}
+		
+		if(Input.GetAxisRaw("5th axis")> 0.3|| Input.GetAxisRaw("5th axis") < -0.3)
+		{
+			currentAxis = "5th axis";
+			axisInput = Input.GetAxisRaw("5th axis");
+		}
+		
+		if(Input.GetAxisRaw("6th axis")> 0.3|| Input.GetAxisRaw("6th axis") < -0.3)
+		{
+			currentAxis = "6th axis";
+			axisInput = Input.GetAxisRaw("6th axis");
+		}
+		
+		if(Input.GetAxisRaw("7th axis")> 0.3|| Input.GetAxisRaw("7th axis") < -0.3)
+		{
+			currentAxis = "7th axis";
+			axisInput = Input.GetAxisRaw("7th axis");
+		}
+		
+		if(Input.GetAxisRaw("8th axis") > 0.3|| Input.GetAxisRaw("8th axis") < -0.3)
+		{
+			currentAxis = "8th axis";
+			axisInput = Input.GetAxisRaw("8th axis");
+		}*/
+		
+		
+		
+		// 4 - Movement per direction
+		movement = new Vector2(
+			speed.x * inputX,
+			speed.y * inputY);
+		
+	}
+
+	
+	void FixedUpdate()
+	{
+		// 5 - Move the game object
+		rigidbody2D.velocity = movement;
+	}
+	
+}
