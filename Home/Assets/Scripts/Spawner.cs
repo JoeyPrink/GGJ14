f3fa29e94ada012c10daf2cfb6922d633a555787
Samuel Kogler <samuel.kogler@gmail.com>
2014-01-25 21:23:40 +0100

    stuff

--- Home/Assets/Scripts/Spawner.cs
+++ Home/Assets/Scripts/Spawner.cs
@@ -3,28 +3,19 @@ using System.Collections;
 
 public class Spawner : MonoBehaviour
 {
-	public float spawnTime = 5f;		// The amount of time between each spawn.
-	public float spawnDelay = 3f;		// The amount of time before spawning starts.
-	public GameObject[] enemies;		// Array of enemy prefabs.
 
+	GameObject go = (GameObject)Instantiate(Resources.Load("Stein"));
 
 	void Start ()
 	{
-		// Start calling the Spawn function repeatedly after a delay .
-		InvokeRepeating("Spawn", spawnDelay, spawnTime);
+		InvokeRepeating("Spawn", 16f, 6f);
 	}
 
 
 	void Spawn ()
 	{
-		// Instantiate a random enemy.
-		int enemyIndex = Random.Range(0, enemies.Length);
-		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
+		go = (GameObject)Instantiate(Resources.Load("Stein"));
+		Instantiate(go, transform.position, transform.rotation);
 
-		// Play the spawning effect from all of the particle systems.
-		foreach(ParticleSystem p in GetComponentsInChildren<ParticleSystem>())
-		{
-			p.Play();
-		}
 	}
 }
